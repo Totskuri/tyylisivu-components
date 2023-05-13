b23e88cc5d6a478d99419fc2af98e37a24b3144f
Joni Uusimäki <herravoima@gmail.com>
2023-05-13 23:41:37 +0300

    jsdoc

--- src/components/form/Checkbox/Checkbox.tsx
+++ src/components/form/Checkbox/Checkbox.tsx
@@ -3,6 +3,14 @@ import '../../../css/variables.css';
 import classNames from "../../../utils/classNames";
 import {CheckboxProps} from "./Checkbox.types";
 
+/**
+ * @param checked
+ * @param onChange
+ * @param disabled
+ * @param className
+ * @param style
+ * @constructor
+ */
 const TextInput = ({
     checked,
     onChange,
