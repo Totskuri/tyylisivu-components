b05f318ef03c8868bdcf9eab7516cadef80fbbcb
Joni Uusimäki <herravoima@gmail.com>
2023-09-02 20:55:03 +0300

    vh to dvh

--- docs/assets/useOnClickOutside-752e6979.js
+++
@@ -1,1 +0,0 @@
-import{r as o}from"./index-229f28ec.js";const u=(t,n)=>{const e=r=>{!t||!t.current||t.current.contains(r.target)||n(r)};o.useEffect(()=>(document.addEventListener("mousedown",e),document.addEventListener("touchstart",e),()=>{document.removeEventListener("mousedown",e),document.removeEventListener("touchstart",e)}),[t,n])};export{u};
